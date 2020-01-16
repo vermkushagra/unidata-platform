ad7d16721176b690d6585744a98ded9e1dd9ac5d
Alekxander Malyshev <alexander.malyshev@unidata-platform.ru>
2020-01-16 16:35:00 +0300

    Add license header to java files.

--- org.unidata.mdm.meta/src/main/java/org/unidata/mdm/meta/service/impl/MetaModelImportServiceImpl.java
+++ org.unidata.mdm.meta/src/main/java/org/unidata/mdm/meta/service/impl/MetaModelImportServiceImpl.java
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
-
 package org.unidata.mdm.meta.service.impl;
 
 import org.apache.commons.collections4.CollectionUtils;
